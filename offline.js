66ae096522823eea592a272c08f2039794710180
stristra2020 <154002881+stristra2020@users.noreply.github.com>
2024-12-29 14:22:34 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1735463362,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/небо-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/Алекс-sheet0.png",
+		"images/земля.png",
+		"images/верх-sheet0.png",
+		"images/Спрайт-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
